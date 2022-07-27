21387298224d1558e07c5a622d7f2a9b946ae1f2
galer7 <gabigaler50@gmail.com>
2022-07-27 17:21:51 +0300

    Initial setup

---
+++ index.ts
@@ -0,0 +1,135 @@
+import { Server, Socket } from "socket.io";
+import { prisma } from "./prisma/client";
+import superjson from "superjson";
+import { FoodieGroup } from "@prisma/client";
+const io = new Server(3001, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const persistStateChangeAsync = async (foodieGroupMap, foodieGroupId) => {
+  await prisma.foodieGroup.update({
+    where: { id: foodieGroupId },
+    data: { foodieGroupState: superjson.stringify(foodieGroupMap) },
+  });
+};
+
+const m: Map<string, Map<string, Array<object>>> = new Map();
+
+(async () => {
+  const allFoodieGroups = await prisma.foodieGroup.findMany();
+  allFoodieGroups.forEach(
+    ({ id, foodieGroupState: stringifiedFoodieGroupState }: FoodieGroup) => {
+      if (!stringifiedFoodieGroupState) return;
+      m.set(id, superjson.parse(stringifiedFoodieGroupState));
+    }
+  );
+  console.log(m);
+
+  io.on("connection", (socket: Socket) => {
+    socket.on("user:first:render", (foodieGroupId) => {
+      const foodieGroupMap: Map<string, object> | undefined =
+        m.get(foodieGroupId);
+
+      console.log({ foodieGroupMap });
+
+      socket.join(foodieGroupId);
+      socket.emit("server:first:render", superjson.stringify(foodieGroupMap));
+    });
+
+    socket.on(
+      "user:invite:sent",
+      async (from, to, foodieGroupId, fromUserState) => {
+        // create room on first group invite sent
+        console.log("received user:invite:sent", {
+          from,
+          to,
+          foodieGroupId,
+          fromUserState,
+        });
+
+        // send to all users ever unfortunately
+        io.emit("server:invite:sent", from, to, foodieGroupId);
+        socket.join(foodieGroupId);
+
+        // if it is the first invite, the sender sends its user state also
+        if (!m.has(foodieGroupId)) {
+          m.set(
+            foodieGroupId,
+            new Map([
+              [from, fromUserState],
+              [to, []],
+            ])
+          );
+        } else {
+          m.get(foodieGroupId).set(to, []);
+        }
+        await persistStateChangeAsync(m.get(foodieGroupId), foodieGroupId);
+
+        console.log("map after invite sent", m);
+      }
+    );
+
+    socket.on(
+      "user:invite:accepted",
+      async (name, foodieGroupId, userState) => {
+        // add socket which accepted the invite to the room
+        socket.join(foodieGroupId);
+
+        // update group state so that we can render RT updates
+        const foodieGroupMap: Map<string, object> | undefined =
+          m.get(foodieGroupId);
+        if (!foodieGroupMap)
+          console.log(`user ${name} does not exist on FG ${foodieGroupId}`);
+        foodieGroupMap.set(name, userState);
+        await persistStateChangeAsync(foodieGroupMap, foodieGroupId);
+
+        // TODO: Create here the foodieGroup in the DB, not from the next.js
+
+        foodieGroupMap.forEach((userState, name) => {
+          io.to(foodieGroupId).emit(
+            "server:state:updated",
+            superjson.stringify(userState),
+            name
+          );
+        });
+      }
+    );
+
+    socket.on("user:state:updated", async (name, foodieGroupId, userState) => {
+      console.log("received user:state:updated event", {
+        name,
+        foodieGroupId,
+        userState,
+      });
+
+      console.log(m);
+      // update group state so that we can render RT updates
+      if (!m.get(foodieGroupId)) {
+        console.log(`user ${name} does not exist on FG ${foodieGroupId}`);
+        // TODO: remove this, it should theoretically exist already
+        m.set(foodieGroupId, new Map());
+      }
+      const foodieGroupMap: Map<string, object> | undefined =
+        m.get(foodieGroupId);
+      foodieGroupMap.set(name, userState);
+      await persistStateChangeAsync(foodieGroupMap, foodieGroupId);
+
+      console.log(m);
+
+      console.log("emit server:state:updated", [
+        superjson.stringify(foodieGroupMap.get(name)),
+        name,
+      ]);
+
+      io.to(foodieGroupId).emit(
+        "server:state:updated",
+        superjson.stringify(userState),
+        name
+      );
+    });
+  });
+
+  console.log("registered all handlers!");
+})();
